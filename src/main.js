be4d75cc4100aa0b5049eb0d83fc5a2f305fffce
define2029 <3122203641@qq.com>
2025-04-24 17:15:03 +0800

    删除无关文件

--- src/main.js
+++ src/main.js
@@ -10,16 +10,6 @@ import '@/assets/main.css'
 
 const app = createApp(App)
 
-// 引入全局方法
-import * as utils from '@/utils'
-app.config.globalProperties.$utils = utils
-
-// SVG 组件自动注册脚本
-import 'virtual:svg-icons-register'
-// 全局注册 SVG 组件
-import SvgIcon from '@/components/SvgIcon/index.vue'
-app.component('svg-icon', SvgIcon)
-
 app.use(createPinia())
 app.use(router)
 
